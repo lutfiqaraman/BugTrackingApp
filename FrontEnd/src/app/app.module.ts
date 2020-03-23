9e96cadb420632eb3a504a7fe4ac72d0d3243f25
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-23 22:51:37 +0200

    create an issue component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BugsComponent } from './components/bugs/bugs.component';
+import { IssuesComponent } from './components/issues/issues.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BugsComponent,
+    IssuesComponent
   ],
   imports: [
     BrowserModule,
