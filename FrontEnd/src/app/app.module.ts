42c51502f617a008173d1f26b6b1dc09112dd660
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-23 22:55:57 +0200

    remove the import of unknown component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BugsComponent } from './components/bugs/bugs.component';
 import { IssuesComponent } from './components/issues/issues.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BugsComponent,
     IssuesComponent
   ],
   imports: [
