10783b577bc15aec7c63aed259f5c7e282aca32d
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-24 23:03:10 +0200

    create a menu

--- FrontEnd/src/app/angularmaterial/material.module.ts
+++ FrontEnd/src/app/angularmaterial/material.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatMenuModule
   ],
   exports: [
-    MatGridListModule
+    MatGridListModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule { }
